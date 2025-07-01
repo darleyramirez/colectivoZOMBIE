bed6ab1a4382826636879008678ed0ac2a691034
Darley Ramirez Garzon <darley.ramirez@globant.com>
2025-07-01 06:55:26 -0500

    feat se crean variables

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,33 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner Keyentry= new Scanner(System.in);
+
+        System.out.println("***** RESIDENT EVIL BY GLOBERS ULTIMATE *****");
+
+        //Caracteristicas Leon s Kennedy
+        //Nombre
+        //Edad
+        //CantidadVida
+
+        // Personaje MR X (Titan)
+        //Nombre
+        //Virus que lo infecto
+        //CantidadVIDA
+
+        final String nombre;
+        final Byte edad;
+        int cantidadVIDAD;
+        final String nombrex;
+        final Byte edadx;
+        int cantidadVIDADX;
+
+
 
 
 
